5012775a72729a3cd2e6daca2b2464e5f487d84e
Тихонова Ярослава ТКИ-142 <yaroslava.tikhonova.08@mail.ru>
2026-09-04 15:35:58 +0300

    Update point3.cpp

--- point3.cpp
+++ point3.cpp
@@ -8,9 +8,9 @@ double Point::getX() const { return x; }
 double Point::getY() const { return y; }
 double Point::getZ() const { return z; }
 
-void Point::setX(double x) { this->x = x; }
-void Point::setY(double y) { this->y = y; }
-void Point::setZ(double z) { this->z = z; }
+void Point::setX(const double x) { this->x = x; }
+void Point::setY(const double y) { this->y = y; }
+void Point::setZ(const double z) { this->z = z; }
 
 ostream& operator<<(ostream& os, const Point& point)
 {
