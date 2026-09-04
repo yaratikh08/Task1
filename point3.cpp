9bc9a3cfcfd6f41b73f344f045ced17cfcbdb855
Тихонова Ярослава ТКИ-142 <yaroslava.tikhonova.08@mail.ru>
2026-09-04 15:27:32 +0300

    Update point3.cpp

--- point3.cpp
+++ point3.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-Point::Point(double x, double y, double z) : x(x), y(y), z(z) {}
+Point::Point(const double x,const double y,const double z) : x(x), y(y), z(z) {}
 
 double Point::getX() const { return x; }
 double Point::getY() const { return y; }
